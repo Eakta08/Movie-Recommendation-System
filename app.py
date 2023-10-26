cfcd005fb7df04998ff9567ee8931304657a7378
“Eakta <eaktaprasad08@gmail.com>
2023-10-26 20:57:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-
 import pickle
 import streamlit as st
 import requests
-
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
@@ -13,12 +11,11 @@ def fetch_poster(movie_id):
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
-
 def get_recommendations(movie):
     idx = movies[movies['original_title'] == movie].index[0]
     sim_scores = list(enumerate(similarity[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:6]
+    sim_scores = sim_scores[1:11]
     # Top 5 similar items (excluding itself)
     recommended_movie_names = []
     recommended_movie_posters = []
@@ -28,7 +25,6 @@ def get_recommendations(movie):
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].original_title)
     return recommended_movie_names, recommended_movie_posters
-
 
 st.header('Movie Recommender System')
 movies = pickle.load(open('movie_list.pkl', 'rb'))
@@ -42,6 +38,8 @@ selected_movie = st.selectbox(
 
 if st.button('Show Recommendation'):
     recommended_movie_names, recommended_movie_posters = get_recommendations(selected_movie)
+
+    # Create two rows with 5 columns each
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
@@ -49,14 +47,30 @@ if st.button('Show Recommendation'):
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
     with col4:
         st.text(recommended_movie_names[3])
         st.image(recommended_movie_posters[3])
-
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
+
+    # Create a second row
+    col6, col7, col8, col9, col10 = st.columns(5)
+    with col6:
+        st.text(recommended_movie_names[5])
+        st.image(recommended_movie_posters[5])
+    with col7:
+        st.text(recommended_movie_names[6])
+        st.image(recommended_movie_posters[6])
+    with col8:
+        st.text(recommended_movie_names[7])
+        st.image(recommended_movie_posters[7])
+    with col9:
+        st.text(recommended_movie_names[8])
+        st.image(recommended_movie_posters[8])
+    with col10:
+        st.text(recommended_movie_names[9])
+        st.image(recommended_movie_posters[9])
